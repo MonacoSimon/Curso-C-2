59a68358bcc1f3a01b50b7a5138abeb18ec5051e
SimonM <simonmonaco86@gmail.com>
2024-05-28 15:28:30 -0300

    arquitectura en capas

--- Ejemplos-ado-net/Dominio/Properties/Elemento.cs
+++ Ejemplos-ado-net/Dominio/Properties/Elemento.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ejemplo_Pokemons
+namespace Dominio
 {
-    class Elemento
+    public class Elemento
     {
         public int Id { get; set; }
         public string Descripcion { get; set; }
